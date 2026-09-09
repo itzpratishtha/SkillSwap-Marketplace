dcb0676ab7b2e2fff2e93296cced30972e4b7115
pratishtha <pratishthasomani19@gmail.com>
2026-09-10 00:06:18 +0530

    Fix SMTP configuration for production

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 import cors from "cors";
 import dotenv from "dotenv";
 
--- server/services/email.service.js
+++ server/services/email.service.js
@@ -6,14 +6,18 @@ dotenv.config({
 });
 
 const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
 
-    service: "gmail",
+    port: 587,
+
+    secure: false,
+
+    requireTLS: true,
 
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASSWORD,
     },
-
 });
 
 
